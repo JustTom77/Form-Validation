4209288f9b0c94bc1a5a2fa24354a76956e5f36c
JustTom77 <20codetom20@gmail.com>
2021-05-10 15:58:37 +0200

    finish project

---
+++ script.js
@@ -0,0 +1,65 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm() {
+  // Using Contraint API
+  isValid = form.checkValidity();
+  // STYLE MAIN MESSAGE FOR AN ERROR
+  if (!isValid) {
+    message.textContent = "Please fill out all fields!";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  // CHECK TO SEE IF PASSWORDS MATCH
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure passwords match!";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  // IF FORM IS VALID AND PASSWORDS MATCH
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
+}
+
+function storeFomData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // DO SOMETHING WITH USER DATA
+  console.log(user);
+}
+
+function proccesFormData(e) {
+  e.preventDefault();
+  // VALIDATE FORM
+  validateForm();
+  // SUBMIT DATA IF VALID
+  if (isValid && passwordsMatch) {
+    storeFomData();
+  }
+}
+
+// ==> EVENT LISTENER <==
+form.addEventListener("submit", proccesFormData);
